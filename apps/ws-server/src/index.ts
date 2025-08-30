b42c7a5ef654e7999a8fc109d4c4a759bc88329d
Sahil-Shadwal <sahilshadwal@gmail.com>
2025-08-30 18:44:20 +0530

    completed the basic turborepo app

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -4,12 +4,13 @@ const server = new WebSocketServer({
     port: 3001,
 });
 
-server.on("connection", (socket) => {
-    client.user.create({
+server.on("connection", async (socket) => {
+    await client.user.create({
         data: {
             name: Math.random().toString(),
             password: Math.random().toString(),
         },
     });
+
     socket.send("Hii there you are connected to the server!");
 });
